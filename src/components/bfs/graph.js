4be714560fbc7a0ff782f7704fae6e41340f79d6
Slotigork <maxsoneback@hotmail.com>
2019-01-28 18:55:56 +0100

    set

--- src/components/bfs/graph.js
+++ src/components/bfs/graph.js
@@ -1,3 +1,12 @@
 function Graph(){
   this.queue=[];
+  this.nodes=[];
+  yhis.graph={};
+}
+
+Graph.prototype.addNode(node){
+  this.nodes.push(node);
+  let title = node.value;
+  this.graph[title]= node;
+
 }
--- src/components/bfs/node.js
+++ src/components/bfs/node.js
@@ -1,10 +1,11 @@
 function Node(value){
   this.value=value;
+  this.picture=null;
   this.parent=null;
   this.checked=false;
   this.edges=[];
 }
 
 Node.prototype.addEdge = function(node){
-    this.edges.push(node);  
+    this.edges.push(node);
 }
